1a1fdd6ca6829f3fc04a060dfbc63ab4dbe7fd19
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-02-11 20:28:57 +0800

    initialize Next.js project with Tailwind CSS, ESLint, and basic SVG assets

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const App = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/765ee240-1d37-44d9-ad0e-4ea07b836f49/5GwM4xmz1Y.lottie"
+      loop
+      autoplay
+    />
+  );
+};
+
+export default App;
